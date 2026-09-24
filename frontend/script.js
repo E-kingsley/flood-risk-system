1552094af142d4b8ebf8241c6a90cb6029dfcb54
Ekeoma Chukwuemeka Kingsley <kingsleydaniel001@gmail.com>
2026-09-24 16:07:14 +0100

    Use flyToBounds for a slower, more cinematic map zoom

--- frontend/script.js
+++ frontend/script.js
@@ -202,7 +202,7 @@ function highlightLga(lgaName, riskClass, confidence = 1) {
   map.flyToBounds(layer.getBounds(), {
     padding: [60, 60],
     maxZoom: 11,
-    duration: 2.5,
+    duration: 6,
   });
 
   currentLgaLayer = layer;
